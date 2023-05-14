e3609cf815aa71f6c137a8d5b6e06ff96ea25ea1
fridenity0777 <fridenity0777@proton.me>
2023-05-14 14:42:03 +0700

    2023-05-14

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,20 @@
-return { -- this table overrides highlights in all themes
+return {
+  -- this table overrides highlights in all themes
   -- Normal = { bg = "#000000" },
+  -- apply highlight group to all colorschemes (include the default_theme)
+  -- set the transparency for all of these highlight groups
+  Normal = { bg = "NONE", ctermbg = "NONE" },
+  NormalNC = { bg = "NONE", ctermbg = "NONE" },
+  CursorColumn = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+  CursorLine = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+  CursorLineNr = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+  LineNr = {},
+  SignColumn = {},
+  StatusLine = {},
+  NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
+  NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
+
+  require("notify").setup({
+    background_colour = "#000000",
+  })
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,16 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+   -- You can also add new plugins here as well:
+   -- Add plugins, the lazy syntax
+   { "andweeb/presence.nvim",                lazy = false },
+   { "fladson/vim-kitty",                    lazy = false },
+   { "theRealCarneiro/hyprland-vim-syntax",  lazy = false },
+   { "VebbNix/lf-vim",                       lazy = false },
+   { "LhKipp/nvim-nu",                       lazy = false },
+   -- {
+   --   "ray-x/lsp_signature.nvim",
+   --   event = "BufRead",
+   --   config = function()
+   --     require("lsp_signature").setup()
+   --   end,
+   -- },
 }
